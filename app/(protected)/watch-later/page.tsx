660a5e34c4241593f862cb14f311dc9e4a984717
Sietra <canercanturk2004@gmail.com>
2024-09-11 13:20:32 +0300

    Update page.tsx

--- app/(protected)/watch-later/page.tsx
+++ app/(protected)/watch-later/page.tsx
@@ -2,7 +2,7 @@ import { YouTubeCollections } from "@/components/youtube-collections";
 
 export default function WatchLaterPage() {
   return (
-    <main className="min-h-screen p-4">
+    <main className="min-h-screen p-4 pt-20">
       <YouTubeCollections />
     </main>
   );
